39c5af848fef9e9c02d10fea6a71d8adaba69e5c
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-10 23:55:12 -0300

    card to display in masonry

--- src/app/ui/article-card.tsx
+++ src/app/ui/article-card.tsx
@@ -1,15 +1,18 @@
-export const Card = () => {
+import Image from 'next/image'
+
+export const Card = ({index}: {index: number}) => {
   return (
-    <article className='rounded min-h-[350px] w-[300px] bg-berkeley-blue'>
-      <figure className='h-[150px] w-[300px] bg-ghost-white rounded-t'>
+    <article className='rounded bg-[#00183d] relative break-inside-avoid-column'>
+      <figure className='w-full bg-ghost-white rounded-t relative'>
+        <img src={`https://source.unsplash.com/random/${index}`} alt='random unsplash image' className='h-auto rounded-t' />
       </figure>
       <section className='px-3 pt-3 flex flex-col'>
-        <p>Title</p>
+        <p>Title {index}</p>
         <small className='mt-1'>By: Author</small>
         <p className='mt-3'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
         </p>
-        <a className='my-5 rounded p-1.5 bg-indigo text-center w-1/2 hover:scale-105 transition-all cursor-pointer'>Read More</a>
+        <a  className='my-5 rounded p-1.5 bg-indigo text-center w-1/2 hover:scale-105 transition-all cursor-pointer'>Read More</a>
       </section>
     </article>
   )
