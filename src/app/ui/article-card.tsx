a257bcadf5fb4b1c380386f3f8140b17d03def57
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-11 02:47:08 -0300

    use color thief pkg to set glass element bg

--- src/app/ui/article-card.tsx
+++ src/app/ui/article-card.tsx
@@ -1,4 +1,8 @@
+'use client'
+import { useColor } from 'color-thief-react'
+
 export const Card = ({index}: {index: number}) => {
+  const { data } = useColor(`https://source.unsplash.com/random/${index}`, 'rgbArray', { crossOrigin: 'Anonymous' })
   return (
     <article className='bg-[#00183d] group bg-inherit relative break-inside-avoid-column mb-4'>
       <figure className='w-full relative'>
@@ -7,7 +11,7 @@ export const Card = ({index}: {index: number}) => {
       <div className='inset-0 absolute grid place-content-center group-hover:invisible'>
         <span className='text-xl font-semibold'>Title {index}</span>
       </div>
-      <div className={`invisible opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity absolute inset-0 m-auto rounded-lg w-[90%] h-[85%] blur-sm bg-sapphire/[0.17] backdrop-blur-sm shadow-[-13px_15px_18px_0px_rgba(0,0,0,0.4)]`}></div>
+      <div className={`invisible opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity absolute inset-0 m-auto rounded-lg w-[90%] h-[85%] blur-sm bg-[${data ? data[0] : 0}, ${data ? data[1] : 0}, ${data ? data[2] : 0}]/[0.25] backdrop-blur-sm shadow-[-13px_15px_18px_0px_rgba(${data ? data[0] : 0}, ${data ? data[1] : 0}, ${data ? data[2] : 0},0.4)]`}></div>
     </article>
   )
 }
