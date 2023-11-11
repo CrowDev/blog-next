e58e3ed5d7e8532710284be3d98d64e20f30df68
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-11 00:46:03 -0300

    updated element hover glass

--- src/app/ui/article-card.tsx
+++ src/app/ui/article-card.tsx
@@ -2,18 +2,11 @@ import Image from 'next/image'
 
 export const Card = ({index}: {index: number}) => {
   return (
-    <article className='rounded bg-[#00183d] relative break-inside-avoid-column'>
+    <article className='rounded bg-[#00183d] relative break-inside-avoid-column group mb-4'>
       <figure className='w-full bg-ghost-white rounded-t relative'>
         <img src={`https://source.unsplash.com/random/${index}`} alt='random unsplash image' className='h-auto rounded-t' />
       </figure>
-      <section className='px-3 pt-3 flex flex-col'>
-        <p>Title {index}</p>
-        <small className='mt-1'>By: Author</small>
-        <p className='mt-3'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-        </p>
-        <a  className='my-5 rounded p-1.5 bg-indigo text-center w-1/2 hover:scale-105 transition-all cursor-pointer'>Read More</a>
-      </section>
+      <div className='invisible group-hover:visible transition-[visibility] absolute inset-0 m-auto rounded w-[90%] h-[85%] blur-sm bg-sapphire/[0.17] backdrop-blur-sm shadow-[-13px_15px_18px_0px_rgba(0,0,0,0.4)]'></div>
     </article>
   )
 }
