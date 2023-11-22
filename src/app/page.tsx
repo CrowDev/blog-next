d34f8bf0222ca53c7141ae6cf21e2d30768b2055
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-22 17:05:44 -0300

    using intersection observer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
+'use client'
 import { oxygen } from './ui/fonts'
 import { Search } from './ui/search'
 import { TitleBox } from './ui/title-box'
 import { Card } from './ui/article-card'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
-  return (
-    <main className={`${oxygen.className} font-normal antialiased`}>
+	const elementRef: any = useRef(null)
+	useEffect(() => {
+		const observer = new IntersectionObserver(([el]) => {
+			if (el.isIntersecting) {
+				console.log('intersecting')
+			}
+		}, { rootMargin: '100px' })
+		observer.observe(elementRef.current)
+	}, [])
+
+
+
+  return <main className={`${oxygen.className} font-normal antialiased`}>
       <TitleBox />
       <Search />
-      <section className='p-5 relative columns-2xs gap-8'>
+      <section className='p-5 relative columns-2xs gap-8'  >
         {
           Array(20).fill(0).map((_, i) => (
             <Card key={i} index={i}/>
           ))
         }
       </section>
+			<div ref={elementRef}></div>
     </main>
-  )
+  
 }
