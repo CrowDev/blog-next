23d0c324315f1004785c7e0933b74fe273327338
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-22 19:59:54 -0300

    added interface and updated variables names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,36 @@ import { Card } from './ui/article-card'
 import { useEffect, useRef, useState } from 'react'
 import { useIntersection } from '@/hooks/useIntersection'
 
+interface Element {
+  id: number
+  value: number
+}
+
 export default function Home() {
-	const [elements, setElements] = useState<Array<number>>(
-		Array.from({ length: 10 }, (_, i) => i)
+	const [elements, setElements] = useState<Array<Element>>(
+		Array.from({ length: 10 }, (_, i) => ({id: i, value: i}))
 	)
+  const [term, setTerm] = useState('')
 	const [isClose, elementRef] = useIntersection()
 	useEffect(() => {
     if (isClose) {
-      const newElements = Array.from({ length: elements.length + 10 }, (_, i) => i)
+      const newElements = Array.from(
+        { length: elements.length + 10 }, (_, i) => {
+          return {id: i, value: i}
+        }
+      )
       setElements(newElements)
     }
 	}, [isClose])
+
 
   return <main className={`${oxygen.className} font-normal antialiased`}>
       <TitleBox />
       <Search />
       <section className='p-5 relative columns-2xs gap-8'  >
         {
-          elements.map((element) => (
-            <Card key={element} index={element}/>
+          elements.map((element: Element) => (
+            <Card key={element.id} value={element.value} term={term}/>
           ))
         }
       </section>
--- src/app/ui/article-card.tsx
+++ src/app/ui/article-card.tsx
@@ -1,18 +1,18 @@
 'use client'
 import { useColor } from 'color-thief-react'
 
-export const Card = ({index}: {index: number}) => {
-  const { data } = useColor(`https://source.unsplash.com/random/${index}`, 'rgbArray', { crossOrigin: 'Anonymous' })
+export const Card = ({value, term = ''}: {value: number, term: string}) => {
+  const { data } = useColor(`https://source.unsplash.com/random/${value}`, 'rgbArray', { crossOrigin: 'Anonymous' })
   return (
     <article className='bg-[#00183d] group bg-inherit relative break-inside-avoid-column mb-4'>
       <figure className='w-full relative'>
-        <img src={`https://source.unsplash.com/random/${index}`} alt='random unsplash image' className='h-auto rounded-lg brightness-50 group-hover:brightness-100 transition-[filter]' />
+        <img src={`https://source.unsplash.com/random/${value}`} alt='random unsplash image' className='h-auto rounded-lg brightness-50 group-hover:brightness-100 transition-[filter]' />
       </figure>
       <div className='inset-0 absolute grid place-content-center group-hover:invisible'>
-        <span className='text-xl font-semibold'>Title {index}</span>
+        <span className='text-xl font-semibold'>Title {value}</span>
       </div>
       <div className={`z-10 invisible opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity absolute inset-0 m-auto rounded-lg w-[90%] h-[85%]  bg-[${data ? data[0] : 0}, ${data ? data[1] : 0}, ${data ? data[2] : 0}]/[0.25] backdrop-blur-sm shadow-[-13px_15px_18px_0px_rgba(${data ? data[0] : 0}, ${data ? data[1] : 0}, ${data ? data[2] : 0},0.4)] grid grid-rows-[30%_50%_20%] gap-1 p-3`}>
-        <div className='w-[250px] justify-self-center text-xl font-semibold selection:bg-indigo/[0.6]'>Title {index}</div>
+        <div className='w-[250px] justify-self-center text-xl font-semibold selection:bg-indigo/[0.6]'>Title {value}</div>
         <div className='w-[250px] justify-self-center selection:bg-indigo/[0.6] '>
           <p className='line-clamp-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at sem sit amet enim molestie consequat at eu purus. Phasellus.</p>
         </div>
