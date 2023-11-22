252bd944ae92b911955b17801aa58899ee4bbd47
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-22 19:06:17 -0300

    custom hook for detect closing end imgs

---
+++ src/hooks/useIntersection.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useIntersection = () => {
+	const elementRef: any = useRef(null)
+	const [isClose, setIsClose] = useState(false)
+	useEffect(() => {
+		const observer = new IntersectionObserver(([el]) => {
+			if (el.isIntersecting) {
+				setIsClose(true)
+			} else {
+				setIsClose(false)
+			}
+		}, { rootMargin: '100px' })
+		observer.observe(elementRef.current)
+		return () => observer?.disconnect()
+	}, [])
+
+	return [isClose, elementRef]
+}
